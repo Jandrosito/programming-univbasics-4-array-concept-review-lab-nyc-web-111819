f94d95950640ba8ee868e82899470b91fd2f1173
Edward Castillo <castilloedwarda@gmail.com>
2019-10-25 17:11:42 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -21,5 +21,13 @@ high_num
 end
 
 def find_min_value(array)
-  low_num = array
+   low_num = array[0]
+  counter = 0 
+  while counter < array.length 
+  if array[counter] > low_num
+    low_num = array[counter]
+  end
+  counter += 1
+end
+high_num
 end
